af7fe8572ca0efd4dfaf94570a79d5aa4d7d349b
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2022-04-13 23:34:14 +0300

    animation for check mark in fragment_success.xml more translation

--- app/src/main/java/com/token/quicksell/ui/payment/SuccessFragment.kt
+++ app/src/main/java/com/token/quicksell/ui/payment/SuccessFragment.kt
@@ -1,9 +1,12 @@
 package com.token.quicksell.ui.payment
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.token.quicksell.R
@@ -13,6 +16,7 @@ import com.token.quicksell.databinding.FragmentSuccessBinding
 class SuccessFragment : Fragment() {
 
     lateinit var binding: FragmentSuccessBinding
+    lateinit var check: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +29,26 @@ class SuccessFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.action_successFragment_to_homeFragment)
         )
 
+        check = binding.imageSuccess
+
+        rotateImage()
+
+        scaleImage()
         return binding.root
+    }
+
+    private fun scaleImage() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 2f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 2f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(check, scaleX, scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.start()
+    }
+
+    private fun rotateImage() {
+        val animator: ObjectAnimator = ObjectAnimator.ofFloat(check, View.ROTATION, -360f, 0f)
+        animator.duration = 300
+        animator.start()
     }
 }
